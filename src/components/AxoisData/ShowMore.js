d82cbbc985900d0c013c8d642b041e0670893e3b
bvaibhav23 <vaibhavbhaskar23@gmail.com>
2022-11-17 22:56:41 +0530

    UI update

--- src/components/AxoisData/ShowMore.js
+++ src/components/AxoisData/ShowMore.js
@@ -9,16 +9,6 @@ const ShowMore = (props) => {
             {props.ShowItem.map(({ name, image, price, selling_price, gallery, description }, index) => (
                 <div className="card m-3 shadow rounded p-3 d-flex overflow-auto" key={index}  >
                     <h2 className="card-title ">{name}</h2>
-                    <div className={"card-body  d-flex felx-column "}>
-                        {
-                            gallery.map((val, i) => {
-                                return <div className="d-flex flex-wrap  m-2" key={i}  >
-                                    <img src={val.image} className="shadow border border-dark" alt="" style={{ width: "10rem" }} />
-                                </div>
-
-                            })
-                        }
-                    </div>
                     <div className="d-inline ">
                         <img src={image} className="card-img-top border border-dark" alt={name} style={{ width: "18rem" }} />
 
@@ -33,6 +23,16 @@ const ShowMore = (props) => {
                     <p className="card-text ">Price:<del>₹{price} </del><span className="fw-bold fs-2"> Selling Price: ₹{selling_price}</span></p>
 
                     <p className="d-inline ">{description}</p>
+                    <div className={"card-body  d-flex felx-column "}>
+                        {
+                            gallery.map((val, i) => {
+                                return <div className="d-flex flex-wrap  m-2" key={i}  >
+                                    <img src={val.image} className="shadow border border-dark" alt="" style={{ width: "10rem" }} />
+                                </div>
+
+                            })
+                        }
+                    </div>
 
 
 
