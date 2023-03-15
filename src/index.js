8bebdf5b378ba92a155c4473497d3a3fef57f610
bvaibhav23 <108619671+bvaibhav23@users.noreply.github.com>
2023-03-15 19:56:43 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
-// import 'bootstrap/dist/js/bootstrap.bundle.min';
-import 'bootstrap/js/dist/button';
-import 'bootstrap/js/dist/carousel';
+import 'bootstrap/dist/js/bootstrap.bundle.min';
+// import 'bootstrap/js/dist/button';
+// import 'bootstrap/js/dist/carousel';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from "react-router-dom"
